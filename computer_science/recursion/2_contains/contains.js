da3c307cf46be2af22f4327fcfc781e01162dc65
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-06-21 21:23:08 +0800

    solve contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,23 @@
-const contains = function() {
-  
-};
+const contains = function(key, value) {
+ let valuesObj = Object.values(key);
+
+   for (const item of valuesObj){
+      
+      if(Object.is(item, value)){
+         return true; 
+      }
+
+      if(typeof item === "object" && item !== null){
+         let found = contains(item, value);
+         if(found){
+            return true
+         }
+      }
+   }
+
+   return false;
+
+ }
   
 // Do not edit below this line
 module.exports = contains;
