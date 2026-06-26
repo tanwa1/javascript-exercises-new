9b4985a62fdca3592fb83cc8262bd296e543fc3f
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-06-26 17:52:12 +0800

    solved

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,24 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (objects) {
+
+  if (objects === null || (!Array.isArray(objects) && typeof objects !== "object")) {
+    return undefined;
+  }
+
+  const values = Object.values(objects);
+
+  const nestedArrays = values.reduce((total, currentValue) => {
+     if(Number.isInteger(currentValue)){
+        return total + 1;
+     }
+     else if(Array.isArray(currentValue) || (typeof currentValue === 'object' && currentValue !== null && !Array.isArray(currentValue))){
+        return total + totalIntegers(currentValue)
+     }
+     else{
+        return total + 0;
+     }
+  }, 0);
+       return nestedArrays;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
