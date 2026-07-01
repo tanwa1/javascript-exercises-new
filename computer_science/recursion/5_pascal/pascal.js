f88d31e8d21e170517bceff43516ff670b8b2024
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-07-01 19:15:06 +0800

    solved ;-;

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,19 @@
-const pascal = function() {
-  
+const pascal = function (rows) {
+  const output = [];
+
+  const row = rows - 1;
+  for (let j = 0; j <= row; j++) {
+    output.push(pascalNum(row, j));
+  }
+  return output;
 };
-  
+
+function pascalNum(row, column) {
+  if (column === 0 || column === row) {
+    return 1;
+  } else {
+    return pascalNum(row - 1, column - 1) + pascalNum(row - 1, column);
+  }
+}
 // Do not edit below this line
 module.exports = pascal;
